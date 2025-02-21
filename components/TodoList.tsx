56ad5bbf51e15c28054d40b1f0a6135171bcd19f
saerom-hong <saeromhong12@gmail.com>
2025-02-21 15:05:36 +1300

    add middleware and api

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -3,9 +3,9 @@ import Todo from './Todo';
 const TodoList = ({ todos }) => {
   return (
     <div>
-      {todos.map((todo) => {
-        <Todo todo={todo} key={todo.id} />;
-      })}
+      {todos.map((todo) => (
+        <Todo todo={todo} key={todo.id} />
+      ))}
     </div>
   );
 };
